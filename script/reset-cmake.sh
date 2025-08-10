ed7a6e09d6792ce3a1c9594919c606801aadd23a
delononliu <delononliu@gmail.com>
2025-08-10 15:02:04 +0800

    modify script

--- script/reset-cmake.sh
+++ script/reset-cmake.sh
@@ -1,7 +1,9 @@
-git reset --hard
+git reset .
+git checkout  .
+git clean -fd .
+
+rm -rf .afunc
+cp -rf ../../afunc .afunc
 
 rm CMakeLists.txt -rf
 rm -rf _tmp_workspace/*
-
-mkdir -p _tmp_workspace/make-to-cmake-migration/
-mkdir -p _tmp_workspace/temp-task/
